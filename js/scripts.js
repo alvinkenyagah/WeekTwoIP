84a13de4239e9be283ce4fe2d6e00497217e43d9
Alvinkenyagah <alvin47k@gmail.com>
2022-03-07 06:37:01 +0300

    add year selector

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-const yearSelect = getElementById("year");
-const monthSelect = getElementById("month");
-const daySelect = getElementById("day");
+var min = 1900,
+  max = 2100,
+  select = document.getElementById("year");
+
+for (var i = min; i <= max; i++) {
+  var opt = document.createElement("option");
+  opt.value = i;
+  opt.innerHTML = i;
+  select.appendChild(opt);
+}
