7cbc15baaa6a85d6f915c013bf5204c797065191
Alvinkenyagah <alvin47k@gmail.com>
2022-03-07 07:45:29 +0300

    add dropdown day selector

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
+//Drop down year selector
 var min = 1900,
   max = 2100,
   select = document.getElementById("year");
+
+for (var i = min; i <= max; i++) {
+  var opt = document.createElement("option");
+  opt.value = i;
+  opt.innerHTML = i;
+  select.appendChild(opt);
+}
+var min = 1,
+  max = 31,
+  select = document.getElementById("day");
 
 for (var i = min; i <= max; i++) {
   var opt = document.createElement("option");
